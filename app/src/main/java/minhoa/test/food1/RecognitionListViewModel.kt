3b6e7acad13dc83af2a4d774b01bfbc789741086
Nguyen Thieu Khang <tkhang211@gmail.com>
2021-08-21 15:05:11 +0700

    initial commit
    initial commit

---
+++ app/src/main/java/minhoa/test/food1/RecognitionListViewModel.kt
@@ -0,0 +1,29 @@
+package minhoa.test.food1
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class RecognitionListViewModel : ViewModel(){
+    private val _recognitionList = MutableLiveData<List<Recognition>>()
+    val recognitionList: LiveData<List<Recognition>> = _recognitionList
+
+    fun updateData(recognitions: List<Recognition>){
+        _recognitionList.postValue(recognitions)
+    }
+}
+
+/**
+ * Simple Data object with two fields for the label and probability
+ */
+data class Recognition(val label:String, val confidence:Float) {
+
+    // For easy logging
+    override fun toString():String{
+        return "$label / $probabilityString"
+    }
+
+    // Output probability as a string to enable easy data binding
+    val probabilityString = String.format("%.1f%%", confidence * 100.0f)
+
+}
